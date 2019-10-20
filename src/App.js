376bc2391b4b2f42936788e6c196cf60ef829823
Visakeswaran <vishu@cartoonmango.com>
2019-10-20 21:19:05 +0530

    [Add] - Add Bar chart component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="graph-container">
+        <BarChart
+          width={775}
+          height={244}
+          margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+          data={[
+            { xAxisValue: "India", yAxisValue: 50 },
+            { xAxisValue: "America", yAxisValue: 30 },
+            { xAxisValue: "Australia", yAxisValue: 40 },
+            { xAxisValue: "Pakistan", yAxisValue: 25 },
+            { xAxisValue: "China", yAxisValue: 20 },
+            { xAxisValue: "Russia", yAxisValue: 10 },
+            { xAxisValue: "Africa", yAxisValue: 30 }
+          ]}
+        />
+      </div>
     </div>
   );
 }
